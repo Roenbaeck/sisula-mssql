c0928b6503ccf3b9af0b4cc5250df49dd9bd6e30
Lars Rönnbäck <lars.ronnback@anchormodeling.com>
2025-10-29 16:56:11 +0100

    Simpler conversion

--- templates/CreateTypedTables.sql
+++ templates/CreateTypedTables.sql
@@ -1,25 +1,23 @@
 -- ===== Test template =====
 -- Header
-/*~
--- Generated: $VARIABLES.GENERATED_AT$
--- By: $VARIABLES.USERNAME$ on $VARIABLES.COMPUTERNAME$ ($VARIABLES.USERDOMAIN$)
-CREATE PROCEDURE [$SCHEMA$].[$SOURCE$_CreateTypedTables] AS
+-- Generated: ${VARIABLES.GENERATED_AT}$
+-- By: ${VARIABLES.USERNAME}$ on ${VARIABLES.COMPUTERNAME}$ (${VARIABLES.USERDOMAIN}$)
+CREATE PROCEDURE [${SCHEMA}$].[${SOURCE}$_CreateTypedTables] AS
 BEGIN
     SET NOCOUNT ON;
 
-	$/ foreach t in tables
-	-- Create: $t.table$_Staging
-	CREATE TABLE [$SCHEMA$].[$t.table$_Staging] (
+	$/ foreach t in tables 
+	-- Create: ${t.table}$_Staging
+	CREATE TABLE [${SCHEMA}$].[${t.table}$_Staging] (
 		$/ foreach c in t.columns order by c.ordinal 
-		[$c.name$] $c.type$, -- column number $LOOP.index$ (ordinal $c.ordinal$)
+		[${c.name}$] ${c.type}$, -- column number ${LOOP.index}$ (ordinal ${c.ordinal}$)
 		$/ if LOOP.first 
 		-- that was the first column
 		$/ endif
 		$/ endfor
-		[created_at] datetime2 not null default ''$TIMESTAMP$''
+		[created_at] datetime2 not null default ''${TIMESTAMP}$''
 	);
 	$/ endfor
-~*/
 	-- The End
 END
 GO
